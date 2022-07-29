438a44fc1bb6463d195550ba2ac3ba4d06c8a868
GLOBAL\H498948 <dasari.sowjanyajessy@honeywell.com>
2022-07-29 12:40:39 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExtensionMethodDemo
+{
+    public static class ExtMethodClass
+    {
+        public static string ToCurrency(this string str)
+        {
+            if (str.ToCharArray().All(char.IsDigit))
+                return string.Format("${0}", str);
+            else
+                throw new ArgumentException("Entered String does not contain numbers", str);
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string");
+            string given = Console.ReadLine();
+            string result = ExtMethodClass.ToCurrency(given);
+            Console.WriteLine(result);
+        }
+    }
+}   
